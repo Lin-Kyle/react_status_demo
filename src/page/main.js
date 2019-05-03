d1fe63d8541a3edc051511621a39641a840bd70d
linyukai <linyukai@mizhua.com>
2019-05-03 20:10:27 +0800

    demo

--- src/page/main.js
+++ src/page/main.js
@@ -1,41 +1,19 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useRef } from "react";
 import { hot } from "react-hot-loader";
 
-const initialState = 0;
-function init(initialCount) {
-  return { count: initialCount };
-}
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'increment':
-      return { count: state.count + 1 };
-    case 'decrement':
-      return { count: state.count - 1 };
-    case 'reset':
-      return init(action.payload);
-    default:
-      throw new Error();
-  }
-}
-
-function Counter() {
-  const [state, dispatch] = useReducer(reducer, initialState, init);
+function TextInputWithFocusButton() {
+  const inputEl = useRef(null);
+  const onButtonClick = () => {
+    // `current` 指向已挂载到 DOM 上的文本输入元素
+    inputEl.current.focus();
+  };
   return (
     <div>
-      Count: {state.count}
-      <button
-        onClick={() => dispatch({ type: 'reset', payload: initialCount })}>
-
-        Reset
-      </button>
-      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
-      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      <input ref={inputEl} type="text" />
+      <button onClick={onButtonClick}>Focus the input</button>
     </div>
   );
 }
 
 
-
-
-export default hot(module)(Counter);
+export default hot(module)(TextInputWithFocusButton);
