227b478e07e4dde36028034d13e7471a12f2e689
linyukai <linyukai@mizhua.com>
2019-05-03 12:16:37 +0800

    demo

--- src/page/main.js
+++ src/page/main.js
@@ -1,34 +1,41 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 
-function InheritanceInversionHOC(WrappedComponent) {
-  return class NewComponent extends WrappedComponent {
+function PropsProxyHOC(WrappedComponent) {
+  return class NewComponent extends React.Component {
     render() {
-      console.log(this)
-      return super.render()
+      return this.props.isLoading ? <div>Loading...</div> : <WrappedComponent {...this.props} />
     }
   }
 }
 
 // 被获取ref实例组件
 class Main extends Component {
+  render() {
+    return (
+      <div>Main</div>
+    )
+  }
+}
+
+const HOCComponent = PropsProxyHOC(Main)
+
+class ParentComponent extends Component {
   constructor() {
     super()
     this.state = {
-      name: 'WrappedComponent'
+      isLoading: true
     }
   }
 
-  componentDidMount() {
-    console.log('WrappedComponent did mount')
-  }
-
   render() {
+    setTimeout(() => this.setState({ isLoading: false }), 2000)
     return (
-      <div ref="child">Main</div>
+      <HOCComponent isLoading={this.state.isLoading} />
     )
   }
 }
 
 
-export default hot(module)(InheritanceInversionHOC(Main));
+
+export default hot(module)(ParentComponent);
