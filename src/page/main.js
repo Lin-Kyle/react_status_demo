a0f68c6d787c6cea06cb1948fbe2f3795f4ec1ee
linyukai <linyukai@mizhua.com>
2019-05-03 11:59:17 +0800

    demo

--- src/page/main.js
+++ src/page/main.js
@@ -1,38 +1,49 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 
-
 function PropsProxyHOC(WrappedComponent) {
   return class NewComponent extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = { fields: {} }
+    }
+
+    getField(fieldName) {
+      const _s = this.state
+      if (!_s.fields[fieldName]) {
+        _s.fields[fieldName] = {
+          value: '',
+          onChange: event => {
+            this.state.fields[fieldName].value = event.target.value
+            // 强行触发render
+            this.forceUpdate()
+            console.log(this.state)
+          }
+        }
+      }
+
+      return {
+        value: _s.fields[fieldName].value,
+        onChange: _s.fields[fieldName].onChange
+      }
+    }
+
     render() {
-      const newProps = {}
-      typeof this.props.getInstance === "function" && (newProps.ref = this.props.getInstance)
+      const newProps = {
+        fields: this.getField.bind(this),
+      }
       return <WrappedComponent {...this.props} {...newProps} />
     }
   }
 }
 
+// 被获取ref实例组件
 class Main extends Component {
   render() {
-    return (
-      <div>Main</div>
-    )
+    return <input type="text" {...this.props.fields('name')} />
   }
 }
 
-const HOCComponent = PropsProxyHOC(Main)
 
-class ParentComponent extends Component {
-  getInstance(ref) {
-    this.wrappedInstance = ref;
-    console.log(ref)
-  }
 
-  render() {
-    return (
-      <HOCComponent getInstance={this.getInstance.bind(this)} />
-    )
-  }
-}
-
-export default hot(module)(ParentComponent);
+export default hot(module)(PropsProxyHOC(Main));
