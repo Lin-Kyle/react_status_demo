0345df664a7d818cf1b4cb5694b7f723e9b631a9
linyukai <linyukai@mizhua.com>
2019-05-03 20:08:14 +0800

    demo

--- src/page/main.js
+++ src/page/main.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect, useReducer } from "react";
 import { hot } from "react-hot-loader";
 
-const initialState = { count: 0 };
+const initialState = 0;
+function init(initialCount) {
+  return { count: initialCount };
+}
 
 function reducer(state, action) {
   switch (action.type) {
@@ -9,16 +12,23 @@ function reducer(state, action) {
       return { count: state.count + 1 };
     case 'decrement':
       return { count: state.count - 1 };
+    case 'reset':
+      return init(action.payload);
     default:
       throw new Error();
   }
 }
 
 function Counter() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState, init);
   return (
     <div>
       Count: {state.count}
+      <button
+        onClick={() => dispatch({ type: 'reset', payload: initialCount })}>
+
+        Reset
+      </button>
       <button onClick={() => dispatch({ type: 'increment' })}>+</button>
       <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
     </div>
